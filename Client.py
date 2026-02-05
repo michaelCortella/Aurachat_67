ee0804b4e328f9eaa1b6474467bf9259cb6ebd4f
SurrealEren <eashancoolest@gmail.com>
2026-02-05 11:39:43 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,6 +1,9 @@
 import socket
+<<<<<<< HEAD
 import time
 import psutil
+=======
+>>>>>>> parent of e2af4f4 (memory and network usage fixes)
 
 # 1. Creazione del socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,8 +11,12 @@ client_socket_UDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket_UDP.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 def get_server_address():
+<<<<<<< HEAD
     client_socket_UDP.connect(("255.255.255.255",20405))
     client_socket_UDP.sendto("DISCOVER_MESSAGE".encode(),("255.255.255.255",20405))
+=======
+    client_socket_UDP.sendto("GETSERVERIP".encode(),("255.255.255.255",20405))
+>>>>>>> parent of e2af4f4 (memory and network usage fixes)
     return client_socket_UDP.recvfrom(1024)
 
 # 2. Connessione al server (IP localhost e porta 12345)
